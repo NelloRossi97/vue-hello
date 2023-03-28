f1d312f20ee97fadde0ae6790712a16b683543d9
NelloRossi97 <rossianiello97@gmail.com>
2023-03-28 13:53:52 +0200

    added image and change color on click on h1

--- js/script.js
+++ js/script.js
@@ -3,10 +3,14 @@ const {createApp} = Vue;
 createApp({
     data(){
         return {
-            title: 'prova',
+            title: 'Hello Vue!',
             color: '',
-            fontsize: '',
-            image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.factweavers.com%2Fblog%2Fintroduction-to-vuejs%2F&psig=AOvVaw3GG2a6kLtZb8MyFrnRef1m&ust=1680088389574000&source=images&cd=vfe&ved=2ahUKEwjC-72Ov_79AhUGYPEDHVTHDYoQjRx6BAgAEAo'
+            image: '../img/vuejs.png'
+        }
+    },
+    methods: {
+        changeColor(){
+            this.color = 'green fonts-6'
         }
     }
 }).mount('#app');
